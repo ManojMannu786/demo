bf386c581bac35005754d335b4fe97d2f2d8f2d4
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-09-22 20:28:42 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,24 @@
+/*const ul= document.querySelector('.items');
+
+ul.firstElementChild.textContent='Hello';
+ul.firstElementChild.style.color='green';
+ul.children[1].style.color='yellow';*/
+
+console.log(documnet.domain);
+console.log(document.title);
+console.log(document.URL)
+document.title=hello;
+console.log(document.dooctype)
+console.log(document.head)
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[3]);
+console.log(document.all[3].textconten'hello');
+console.log(document.forms[0]);
+console.log(document.links);
+
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var items = items = document.getElementsByClassName('list-group-item');
+item[1].style.fontweight= 'bold';
+item[1].style.textColor='green';

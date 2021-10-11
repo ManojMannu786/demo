5e707e5746e340071c6eb22c6c951c2400555b31
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-10-11 12:37:26 +0530

    different response for different request url

--- back-end/kickstart with node/request,response.js
+++ back-end/kickstart with node/request,response.js
@@ -1,8 +1,22 @@
 const http = require('http');
 http.c
 const server = http.createServer((req,res)=>{
-    res.write('<html><head></head><body><h1>Welcome to my Node Js project</h1></body></html>');
-    res.end();
+    if(req.url == '/home'){
+        res.write('<html><head></head><body><h1> Welcome home </h1></body></html>');
+        res.end();
+    }
+    else if(req.url == '/about'){
+        res.write('<html><head></head><body><h1> Welcome to About Us page </h1></body></html>');
+        res.end();
+    }
+    else if(req.url == '/node'){
+        res.write('<html><head></head><body><h1> Welcome to my Node Js project </h1></body></html>');
+        res.end();
+    }
+    else {
+        res.write('<html><head></head><body><h1> Welcome to my Node Js project default page </h1></body></html>');
+        res.end();
+    }
 })
 
 server.listen(4000);

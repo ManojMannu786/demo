957697079ebfe09fb187d4a25ee2062c49e04da5
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-10-11 17:51:40 +0530

    clean up code with external file 'routes.js'

--- back-end/kickstart with node/request,response.js
+++ back-end/kickstart with node/request,response.js
@@ -1,34 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req,res)=>{
-    if(req.url == '/'){
-        res.write('<html>')
-        res.write('<head><title> default page </title></head>')
-        res.write('<body><form action ="/message" method = "POST"><input type="text" name="message"><button type="submit"> Send</button></body>')
-        res.write('</html>')
-        return res.end();
-    }
-    if(req.url == '/message' && req.method == 'POST'){
-        const body =[];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-          });
-          req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-          });
-        // fs.writeFileSync('message.txt', 'Dummy Text');
-        res.statusCode= 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.write('<html>')
-        res.write('<head><title> message page </title></head>')
-        res.write('<body><h1>This is message page</h1></body>')
-        res.write('</html>')
-        res.end();
-})
+
+const routes = require('./routes.js');
+
+const server = http.createServer(routes)
 
 server.listen(4000);

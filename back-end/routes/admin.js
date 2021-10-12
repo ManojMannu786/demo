cd86269744b35cf493974e5cebda80a490b870fd
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-10-12 19:59:23 +0530

    add filter and changes redirect to /shop

--- back-end/routes/admin.js
+++ back-end/routes/admin.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const routes = express.Router();
 
+//checks for /admin/add-product => GET
 routes.get('/add-product', (req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="text" name="length"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="product"><input type="text" name="length"><button type="submit">Add Product</button></form>')
 })
-routes.post('/product', (req,res,next)=>{
+//checks for /admin/add-product => POST
+routes.post('/add-product', (req,res,next)=>{
     console.log(req.data);
-    res.redirect('/');
+    res.redirect('/shop');  //redirect to /shop
 })
 
 module.exports = routes;
--- back-end/routes/shop.js
+++ back-end/routes/shop.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const routes = express.Router();
 
+//checks for /shop/ =>get 
 routes.get('/', (req,res,next)=>{
     res.send('<h1> Hello from Manoj </h1>');
 })

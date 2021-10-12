8d873ffefee511a02ea742149133533170d0a4d4
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-10-12 14:39:51 +0530

    body parser

--- back-end/kickstart with node/request,response.js
+++ back-end/kickstart with node/request,response.js
@@ -3,17 +3,23 @@ const http = require('http');
 // const routes = require('./routes.js');
 
 // console.log(routes.text);
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in first middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" title="product"><button type="submit">Add Product</button></form>');
 })
 
-app.use((req,res,next)=>{
-    console.log('inside 2nd middleware');
-    res.send('{ key1: value }');
+app.post('/product', (req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req,res,next)=>{
+    res.send('<h1> Hello from Manoj </h1>')
 })
 
 app.listen(4000);

3d425639a34d126899ab14ea4419b5cccb5f624f
ManojMannu786 <91181620+ManojMannu786@users.noreply.github.com>
2021-10-12 19:28:40 +0530

    adding 404 error page
    res.status(404) sends response code of 404 to client computer

--- back-end/app_express.js
+++ back-end/app_express.js
@@ -16,4 +16,9 @@ const shopRoutes = require('./routes/shop.js');
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+//handeling page not found request
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>:-(</h1><h3>"Page Not Found" : Manoj</h3>')  //res.status(404) sends respnse code of 404
+})
+
 app.listen(4000);
